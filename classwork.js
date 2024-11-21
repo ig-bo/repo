b563959372d8a2235df7747fa3cb5319f21f5b0d
IGBO <38304687+ig-bo@users.noreply.github.com>
2024-11-21 13:27:46 +0100

    Add files via upload

---
+++ classwork.js
@@ -0,0 +1,63 @@
+// let year = prompt("which year are we?",);
+// if (year == 2024) {
+//     alert('you are normal');
+// } else {
+//     alert('you dont have sense');
+// }
+
+// let age = prompt("how old are you?",);
+// if (age == '=<3') {
+//     alert('hi baby');
+// } else if (age <=18) {
+//     alert('hi adult')
+// }
+// else if (age <=100) {
+//     alert('age unknown')
+// }
+
+// let time = prompt("what is the time?",);
+// if (time == '=<8') {
+//     alert('good morning');
+// } else if (time <=12) {
+//     alert ('good afternoon')
+// }
+// else if (time <=16) {
+//     alert ('good evening')
+// }
+// else if (time <=22) {
+//     alert ('good night')
+// }
+
+// let time = prompt('what is the time?',);
+// if (time >= 1 && time <= 12) {
+//     alert('good morning');
+// } else if (time >= 13 && time <= 15) {
+//     alert('good afternoon');
+
+// } else if (time >= 16 && time <= 21) {
+//     alert('good evening');
+// } else if (time >= 22 && time <= 24) {
+//     alert('good night');
+// }
+
+// let time = prompt('what is the time?',);
+// time = (time == 1 || time <= 12) ? alert('Good morning') :
+//     (time > 12 && time <= 16) ? alert('Good afternoon') : (time > 16 && time <= 20)
+//         ? alert('Good evening') : alert('Good night')
+
+// let age = prompt('what is your age?',);
+// age = (age == 3 || age <= 12) ? alert('welcome Child') :
+//     (age > 13 && age <= 18) ? alert('welcome Teen') :
+//         (age > 19 && age <= 50) ? alert('welcome Adult') :
+//         (age > 51 && age <= 100) ? alert('welcome Oldie') :
+//         alert ('Goodbye')
+
+// let divine = prompt('Is Divine Victory Lady ?',);
+// divine = (divine == 'His Lady') ? mgs = 'They will marry after JavaScript' :
+//     mgs = 'They will marry by force';
+// alert(mgs);
+
+let igbo = prompt('is Igbo a twin?');
+igbo = igbo == 'twin'?
+(console.log('yes they are')) :
+(console.log('No'));
---
+++ script.js
@@ -0,0 +1,39 @@
+//alert('I am a Javascript developer');
+//alert(1+2+3);
+//alert('man');
+//alert('woman');
+//alert('children')
+
+//['man', 'woman', 'children'].forEach(alert);
+
+let msg;
+var message;
+
+msg="I will not go home";
+message="do you wanna go home?";
+
+//[msg,message].forEach(alert);
+
+let user = 'Rashford' , age = 40, hobby = 'fighting';
+let about = user + ' at age ' + age + ' still ' + hobby ;
+//alert(about);
+
+//Datatype
+let myNumber = 9072979302;
+numB = 0.008282;
+
+let myName = 'Chima';
+//alert('not a number'/3*3);
+
+let him = 'This man eeh';
+let her = "Leave these girls alone";
+let them = `they like to talk too much`;
+
+let ToriWorWor = `${him} ${her} ${them} like ${5+5} people`
+console.log(ToriWorWor);
+
+let UserName = prompt('What is your Username?' )
+//alert(`my username is ${UserName}`);
+
+let LogOut = confirm("Are you sure you want to logout?")
+alert(LogOut);
